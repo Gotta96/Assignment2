99e21a1ed4dfbdf73863b46134712b53aae7c286
Gotta96 <mattia.gottardello@studenti.unipd.it>
2018-12-13 23:18:09 +0100

    tentativo di aumento della copertura che ora è al 89%

--- src/main/java/it/unipd/tos/App.java
+++ src/main/java/it/unipd/tos/App.java
@@ -5,14 +5,10 @@
 
 package it.unipd.tos;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //System.out.println( "Hello World!" );
     }
 }
